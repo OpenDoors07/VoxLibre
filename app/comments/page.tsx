4f37fbda8b82ad9189e020cc04903ca7d97b4c02
OpenDoors07 <voxlibre@wp.pl>
2025-06-15 01:08:03 +0200

    Add comments and likes pages

---
+++ app/comments/page.tsx
@@ -0,0 +1,8 @@
+export default function CommentsPage() {
+  return (
+    <div>
+      <h1>Komentarze</h1>
+      <p>To jest strona komentarzy.</p>
+    </div>
+  );
+}
---
+++ app/likes/page.tsx
@@ -0,0 +1,8 @@
+export default function LikesPage() {
+    return (
+        <div>
+            <h1>Polubienia</h1>
+            <p>To jest strona z polubieniami.</p>
+        </div>
+    );
+}
